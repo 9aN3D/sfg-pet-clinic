d8030dadf63009c3aac6af481658d51731c69754
Volodymyr Kovalchuk <9an3dw0w@gmail.com>
2018-09-03 19:08:38 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/com/example/guru/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.example.guru.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/example/guru/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.example.guru.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
